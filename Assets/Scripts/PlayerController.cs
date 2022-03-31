82c1428a98d98b56f621a76911097f5a8ba929fb
Sangwook Yoo <tkddnr032@gmail.com>
2022-03-31 22:36:12 +0900

    Feat: Player InputActions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController characterController;
+    public float moveSpeed = 5;
+
+    private Vector2 _moveDir;
+    private CharacterController _characterController;
+    private PlayerInpuActions _playerInputActions;
+    private InputAction _move;
+    private InputAction _fire;
+
+    private void OnEnable()
+    {
+        _move = _playerInputActions.Player.Move;
+        _move.Enable();
+
+        _fire = _playerInputActions.Player.Fire;
+        _fire.Enable();
+        _fire.performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        _move.Disable();
+        _fire.Disable();
+    }
+
+    void Awake()
+    {
+        _playerInputActions = new PlayerInpuActions();
+    }
 
     void Start()
     {
-        characterController = this.gameObject.GetComponent<CharacterController>();
+        _characterController = this.gameObject.GetComponent<CharacterController>();
     }
 
     void Update()
@@ -24,16 +52,18 @@ public class PlayerController : MonoBehaviour
 
     public void Move()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        Vector3 dir = new Vector3(h, 0, v).normalized;
-
-        characterController.Move(dir * Time.deltaTime * 5);
+        _moveDir = _move.ReadValue<Vector2>();
+        Vector3 moveDir = new Vector3(_moveDir.x, 0, _moveDir.y).normalized;
+        _characterController.Move(moveDir * Time.deltaTime * 5);
     }
 
     public void BlendAnimation()
     {
-        
+
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire!");
     }
 }
